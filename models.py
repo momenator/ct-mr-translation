7c99f4587052b077cbafd15cda0e9747a51885bd
momenator <muhammad.rafdi@motius.de>
2019-08-12 12:08:59 +0200

    Added ckpt dir path

--- models.py
+++ models.py
@@ -184,6 +184,7 @@ class CycleGAN:
 
     def save_ckpt(self, epoch, iteration, keep_ckpt=2):
         ckpt_paths = os.listdir(self.ckpt_dir)
+        ckpt_paths = [ self.ckpt_dir + ckpt_path for ckpt_path in ckpt_paths ]
         
         if len(ckpt_paths) >= keep_ckpt:
             # sort by time of creation
