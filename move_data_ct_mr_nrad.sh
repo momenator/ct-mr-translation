093a4cf8befc1ed0588de59df8e9ea1bd704a0c4
momenator <muhammad.rafdi@motius.de>
2019-09-16 18:02:03 +0200

    Script to move data for ct_mr_nrad dataset

--- move_data_ct_mr_nrad.sh
+++ move_data_ct_mr_nrad.sh
@@ -1,16 +1,12 @@
 cp -r ./data/ct_mr_nrad/train/trainA ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/
 cp -r ./data/ct_mr_nrad/train/trainB ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/
 
-cp ./data/ct_mr_nrad/test/10000005_1_CT_wb.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
-cp ./data/ct_mr_nrad/test/10000006_1_CT_wb.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
-cp ./data/ct_mr_nrad/test/10000135_1_CTce_ThAb.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
-cp ./data/ct_mr_nrad/test/10000136_1_CTce_ThAb.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
+cp ./data/ct_mr_nrad/test/20190201_222148cs15T1sagHWSs401a1004.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
+cp ./data/ct_mr_nrad/test/20190203_121518cs15T1sagHWSs401a1004.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
+cp ./data/ct_mr_nrad/test/20190207_224611T1sagBWSs402a1004.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
+cp ./data/ct_mr_nrad/test/20190208_115309cs15T1sagBWSs402a1004.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testA
 
-cp ./data/ct_mr_nrad/test/10000001_3_MRT1_wb.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
-cp ./data/ct_mr_nrad/test/10000003_3_MRT1_wb.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
-cp ./data/ct_mr_nrad/test/10000365_4_MRT1cefs_Ab.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
-cp ./data/ct_mr_nrad/test/10000381_4_MRT1cefs_Ab.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
-
-
-
-
+cp ./data/ct_mr_nrad/test/20190202_124620WirbelsuleKFWirbelsules40200a004.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
+cp ./data/ct_mr_nrad/test/20190207_09360005cThxAbdnativs008a002A.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
+cp ./data/ct_mr_nrad/test/20190211_124950BWSLWSWTKFSpines206a002.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
+cp ./data/ct_mr_nrad/test/20190212_174327AbdpvnormalAbdomens20200a001.npz ../pytorch-CycleGAN-and-pix2pix/datasets/ct_mr_nrad/testB
